c9e8cf5d3ab7ef7f85c103005f4dd42d191d2cc3
nq7304 <43327268+nq7304@users.noreply.github.com>
2018-09-17 16:57:04 +1100

    change to remove javascript from index

--- tabletop.js
+++
@@ -1,8 +0,0 @@
-function init() {
- Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1DuGbygg-r8r8HD2_eIz1TjCB7q2U-pJQGZzC8WEsIqE/pubhtml',
- callback: function(data, tabletop) { 
- console.log(data)
- },
- simpleSheet: true } )
-}
-window.addEventListener(‘DOMContentLoaded’, init)
