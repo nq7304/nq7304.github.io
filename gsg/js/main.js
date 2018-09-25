ff143a8533a7abbeac2ec29bd9042a3c6dc43c4a
nq7304 <43327268+nq7304@users.noreply.github.com>
2018-09-25 16:42:38 +1100

    amend

--- gsg/js/main.js
+++ gsg/js/main.js
@@ -1,5 +1,5 @@
 const app = function () {
-	const API_BASE = 'https://script.google.com/macros/s/AKfycbyP5Rifn7Q05Qcd7CTfm-AOouFHHvUAvCVVuKSfQu-LCqJocP8/exec';
+	const API_BASE = 'https://script.google.com/macros/s/AKfycbxk4ptJ7V-PY9Rw3W0ziq4zbEKGqE_n3GlKiEWBudSnq6_rZSHs/exec';
 	const API_KEY = 'abcdef';
 	const CATEGORIES = ['general', 'financial', 'technology', 'marketing'];
 
